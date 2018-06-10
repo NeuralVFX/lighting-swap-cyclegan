e7dea419c2fb7cd1e23c13df0dadc78b564616f5
NeuralVFX <vfxhoneybadger@gmail.com>
2018-06-10 19:28:46 +0300

    testing sh script

--- data/get_test_dataset.sh
+++ data/get_test_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-URL=https:http://neuralvfx.com/datasets/light_swap/wat_mai_amataros.rar
+URL=http://neuralvfx.com/datasets/light_swap/wat_mai_amataros.rar
 ZIP_FILE=./data/wat_mai_amataros.rar
 TARGET_DIR=./data/wat_mai_amataros/
 wget -N $URL -O $ZIP_FILE
-mkdir wat_mai_amataros
+mkdir TARGET_DIR
 unrar $ZIP_FILE -x $TARGET_DIR=
 rm $ZIP_FILE
